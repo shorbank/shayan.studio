f0c28baa0bcbd22de2f70430828d643d4e0471c4
Shayan <shayanhorbank@gmail.com>
2025-06-14 19:56:27 +0200

    Add: eye model

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,15 +1,17 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+
+function Model() {
+  const { scene } = useGLTF('/models/eye.glb');
+  return <primitive object={scene} />;
+}
 
 export default function Scene() {
   return (
     <Canvas style={{ width: '100%', height: '500px' }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[0, 2, 2]} />
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+      <Model />
       <OrbitControls />
     </Canvas>
   );
