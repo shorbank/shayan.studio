f7a1cf7cd2fd668a2168b88e2503af38ff4b04c0
Shayan <shayanhorbank@gmail.com>
2025-06-21 22:41:03 +0200

    Add test animation

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,18 +1,28 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import { useGLTF, useAnimations } from '@react-three/drei';
+import { useEffect } from 'react';
 
 function Model() {
-  const { scene } = useGLTF('/models/eye.glb');
-  return <primitive object={scene} />;
+  const gltf = useGLTF('/models/eye.glb');
+  const { scene, animations } = gltf;
+  const { actions } = useAnimations(animations, scene);
+
+  useEffect(() => {
+    if (actions && animations.length > 0) {
+      actions[animations[0].name]?.play();
+    }
+  }, [actions, animations]);
+
+  return <primitive object={scene} scale={1} />;
 }
 
 export default function Scene() {
   return (
-    <Canvas style={{ width: '100%', height: '500px' }}>
+    <Canvas style={{ width: '100%', height: '500px' }} camera={{ position: [0, 0, 0.15], fov: 50 }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[0, 2, 2]} />
+      <directionalLight position={[0, 5, 5]} color="fff" />
       <Model />
-      <OrbitControls />
     </Canvas>
   );
 }
